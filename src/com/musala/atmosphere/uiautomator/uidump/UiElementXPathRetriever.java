4ff0d26d032daaa58d3aef66f523384d2e1e6ff1
Denis Bialev <denis.bialev@musala.com>
2015-09-08 13:37:04 +0300

    Implement Xpath Dispatcher.
    Implement xpath dispatcher used for retrieving Accessibility elements using xpath queries.
    
    see #135

---
+++ src/com/musala/atmosphere/uiautomator/uidump/UiElementXPathRetriever.java
@@ -0,0 +1,37 @@
+package com.musala.atmosphere.uiautomator.uidump;
+
+import org.apache.commons.jxpath.JXPathContext;
+
+import com.musala.atmosphere.commons.ui.tree.AccessibilityElement;
+import com.musala.atmosphere.uiautomator.Dispatchable;
+import com.musala.atmosphere.uiautomator.accessibility.AccessibilityFactory;
+import com.musala.atmosphere.uiautomator.helper.AccessibilityHelper;
+import com.musala.atmosphere.uiautomator.xpath.query.QueryExecutor;
+import com.musala.atmosphere.uiautomator.xpath.tree.UiRoot;
+
+import android.view.accessibility.AccessibilityNodeInfo;
+
+/**
+ * Class used to get all {@link AccessibilityElement UI elements} present on the screen and matching a given xpath
+ * query.
+ * 
+ * @author denis.bialev
+ *
+ */
+public class UiElementXPathRetriever implements Dispatchable {
+
+    @Override
+    public Object handle(Object[] args) throws Exception {
+        String xPathQuery = (String) args[0];
+        boolean visibleOnly = (boolean) args[1];
+
+        AccessibilityHelper accessibilityHelper = AccessibilityFactory.getAccessibilityHelper();
+        AccessibilityNodeInfo accessibilityRootNode = accessibilityHelper.getRootInActiveWindow();
+
+        UiRoot uiRoot = AccessibilityFactory.getUiRoot(accessibilityRootNode);
+
+        JXPathContext context = JXPathContext.newContext(uiRoot);
+        QueryExecutor executor = new QueryExecutor(context);
+        return executor.execute(xPathQuery, visibleOnly);
+    }
+}
