44d64194af4950ed5a93bcc3970e93d7526cd145
Denis Bialev <denis.bialev@musala.com>
2015-09-16 13:49:28 +0300

    Created the connection between UiAutomator and Client.
    Replaced the JXpath jar and added third party jar in order to make JXpath
    android compatible. Refactor the UiAutomator build script to use multi dex
    also added a script block that removes the unused jar created during
    building the project. Refactored all Xpath and Css methods to use the new
    logic from UiAutomator.
    
    see #139
    
    Change-Id: I1ab3f6382ed24947aa5a535587665b46e39d9556

--- src/com/musala/atmosphere/uiautomator/uidump/UiElementXPathSuccessorRetriever.java
+++ src/com/musala/atmosphere/uiautomator/uidump/UiElementXPathSuccessorRetriever.java
@@ -12,18 +12,18 @@ import com.musala.atmosphere.uiautomator.xpath.tree.UiRoot;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 /**
- * Class used to get all {@link AccessibilityElement UI elements} present on the screen and matching a given xpath
+ * Class used to get {@link AccessibilityElement UI element}'s children present on the screen and matching a given xpath
  * query.
  * 
  * @author denis.bialev
  *
  */
-public class UiElementXPathRetriever implements Dispatchable {
-
+public class UiElementXPathSuccessorRetriever implements Dispatchable {
     @Override
-    public Object handle(Object[] args) {
+    public Object handle(Object[] args) throws Exception {
         String xPathQuery = (String) args[0];
         Boolean visibleOnly = (Boolean) args[1];
+        AccessibilityElement localRootElement = (AccessibilityElement) args[2];
 
         AccessibilityHelper accessibilityHelper = AccessibilityFactory.getAccessibilityHelper();
         AccessibilityNodeInfo accessibilityRootNode = accessibilityHelper.getRootInActiveWindow();
@@ -33,6 +33,6 @@ public class UiElementXPathRetriever implements Dispatchable {
         JXPathContext context = JXPathContext.newContext(uiRoot);
         QueryExecutor executor = new QueryExecutor(context);
 
-        return executor.execute(xPathQuery, visibleOnly);
+        return executor.executeOnLocalRoot(localRootElement, xPathQuery, visibleOnly);
     }
 }
