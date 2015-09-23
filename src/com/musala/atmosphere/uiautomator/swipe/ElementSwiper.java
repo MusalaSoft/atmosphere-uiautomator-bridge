00c63ff2e40a461f54cd4f457686b326bce701ee
Denis Bialev <denis.bialev@musala.com>
2015-09-23 14:13:55 +0300

    UiScrollableView implements AccessibilityUiElement
    UiScrollableView implements AccessibilityUiElement. Added copy
    constructor in order to reuse logic from Screen. Removed
    AttributeDescriptor. Instead UiElementSelector is used.
    
    see #171
    
    Change-Id: Ic0148755d45cdfe0149765e9f10d321ecdc16d29

--- src/com/musala/atmosphere/uiautomator/swipe/ElementSwiper.java
+++ src/com/musala/atmosphere/uiautomator/swipe/ElementSwiper.java
@@ -5,12 +5,12 @@ import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.core.UiSelector;
 import com.musala.atmosphere.commons.ad.uiautomator.UIAutomatorRequest;
 import com.musala.atmosphere.commons.beans.SwipeDirection;
-import com.musala.atmosphere.commons.ui.UiElementDescriptor;
+import com.musala.atmosphere.commons.ui.UiElementPropertiesContainer;
 import com.musala.atmosphere.uiautomator.Dispatchable;
 import com.musala.atmosphere.uiautomator.util.UiSelectorParser;
 
 /**
- * Class that is responsible for swiping an UI element by it's descriptor.
+ * Class that is responsible for swiping an UI element by it's properties container.
  * 
  * @author yordan.petrov
  * 
@@ -20,10 +20,10 @@ public class ElementSwiper implements Dispatchable {
 
     @Override
     public Object handle(Object[] args) throws UiObjectNotFoundException {
-        UiElementDescriptor descriptor = (UiElementDescriptor) args[0];
+        UiElementPropertiesContainer propertiesContainer = (UiElementPropertiesContainer) args[0];
         SwipeDirection direction = (SwipeDirection) args[1];
 
-        UiSelector selector = UiSelectorParser.convertSelector(descriptor);
+        UiSelector selector = UiSelectorParser.convertSelector(propertiesContainer);
 
         UiObject element = new UiObject(selector);
         switch (direction) {
--- src/com/musala/atmosphere/uiautomator/wait/WaitForUIElementListener.java
+++ src/com/musala/atmosphere/uiautomator/wait/WaitForUIElementListener.java
@@ -3,7 +3,7 @@ package com.musala.atmosphere.uiautomator.wait;
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.core.UiSelector;
-import com.musala.atmosphere.commons.ui.UiElementDescriptor;
+import com.musala.atmosphere.commons.ui.UiElementPropertiesContainer;
 import com.musala.atmosphere.uiautomator.Dispatchable;
 import com.musala.atmosphere.uiautomator.util.UiSelectorParser;
 
@@ -15,14 +15,13 @@ import com.musala.atmosphere.uiautomator.util.UiSelectorParser;
  */
 
 public class WaitForUIElementListener implements Dispatchable {
-
     @Override
     public Object handle(Object[] args) throws UiObjectNotFoundException {
-        UiElementDescriptor descriptor = (UiElementDescriptor) args[0];
+        UiElementPropertiesContainer propertiesContainer = (UiElementPropertiesContainer) args[0];
 
         Integer timeout = (Integer) args[1];
 
-        UiSelector selector = UiSelectorParser.convertSelector(descriptor);
+        UiSelector selector = UiSelectorParser.convertSelector(propertiesContainer);
         UiObject object = new UiObject(selector);
         return object.waitForExists(timeout);
     }
