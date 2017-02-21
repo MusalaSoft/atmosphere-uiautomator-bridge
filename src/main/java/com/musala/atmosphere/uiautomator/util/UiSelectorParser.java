1c2c8471e6e09a20a54c36af216cfe689c8f3a82
Alexander Ivanov <aleksander.ivanov@musala.com>
2017-02-21 10:23:59 +0200

    Fix UiSelector parser to respect the UiElementSelector selection option

--- src/main/java/com/musala/atmosphere/uiautomator/util/UiSelectorParser.java
+++ src/main/java/com/musala/atmosphere/uiautomator/util/UiSelectorParser.java
@@ -2,47 +2,85 @@ package com.musala.atmosphere.uiautomator.util;
 
 import com.android.uiautomator.core.UiSelector;
 import com.musala.atmosphere.commons.ui.UiElementPropertiesContainer;
+import com.musala.atmosphere.commons.ui.selector.CssAttribute;
+import com.musala.atmosphere.commons.ui.selector.UiElementSelectionOption;
+import com.musala.atmosphere.commons.ui.selector.UiElementSelector;
+import com.musala.atmosphere.commons.util.Pair;
 
 /**
  * Utility class responsible for converting {@link UiElementPropertiesContainer} to string and this same string to a
  * {@link UiSelector} instance.
- * 
+ *
  * @author georgi.gaydarov
- * 
+ *
  */
 public class UiSelectorParser {
 
     public static UiSelector convertSelector(UiElementPropertiesContainer propertiesContainer) {
         UiSelector selector = new UiSelector();
         Boolean checked = propertiesContainer.isChecked();
-        String className = propertiesContainer.getClassName();
         Boolean clickable = propertiesContainer.isClickable();
-        String description = propertiesContainer.getContentDescriptor();
         Boolean enabled = propertiesContainer.isEnabled();
         Boolean focusable = propertiesContainer.isFocusable();
         Boolean focused = propertiesContainer.isFocused();
         Integer index = propertiesContainer.getIndex();
         Boolean longClickable = propertiesContainer.isLongClickable();
-        String packageName = propertiesContainer.getPackageName();
         Boolean scrollable = propertiesContainer.isScrollable();
         Boolean selected = propertiesContainer.isSelected();
-        String text = propertiesContainer.getText();
         String resourceId = propertiesContainer.getResourceId();
+
+        if (propertiesContainer instanceof UiElementSelector) {
+            UiElementSelector uiElementSelector = (UiElementSelector) propertiesContainer;
+
+            Pair<String, UiElementSelectionOption> selectorClass = uiElementSelector.getStringValueWithSelectionOption(CssAttribute.CLASS_NAME);
+            Pair<String, UiElementSelectionOption> selectorPackage = uiElementSelector.getStringValueWithSelectionOption(CssAttribute.PACKAGE_NAME);
+            Pair<String, UiElementSelectionOption> selectorText = uiElementSelector.getStringValueWithSelectionOption(CssAttribute.TEXT);
+            Pair<String, UiElementSelectionOption> selectorDescription = uiElementSelector.getStringValueWithSelectionOption(CssAttribute.CONTENT_DESCRIPTION);
+
+            if (selectorClass != null) {
+                selector = setUiSelectorClassNameWithSelectionOption(selector, selectorClass);
+            }
+
+            if (selectorPackage != null) {
+                selector = setUiSelectorPackageWithSelectionOption(selector, selectorPackage);
+            }
+
+            if (selectorText != null) {
+                selector = setUiSelectorTextWithSelectionOption(selector, selectorText);
+            }
+
+            if (selectorDescription != null) {
+                selector = setUiSelectorDescriptionWithSelectionOption(selector, selectorDescription);
+            }
+        } else {
+            String className = propertiesContainer.getClassName();
+            String packageName = propertiesContainer.getPackageName();
+            String text = propertiesContainer.getText();
+            String description = propertiesContainer.getContentDescriptor();
+
+            if (className != null) {
+                selector = selector.className(className);
+            }
+
+            if (packageName != null) {
+                selector = selector.packageName(packageName);
+            }
+
+            if (text != null) {
+                selector = selector.text(text);
+            }
+
+            if (description != null) {
+                selector = selector.description(description);
+            }
+        }
 
         if (checked != null) {
             selector = selector.checked(checked);
         }
 
-        if (className != null) {
-            selector = selector.className(className);
-        }
-
         if (clickable != null) {
             selector = selector.clickable(clickable);
-        }
-
-        if (description != null) {
-            selector = selector.description(description);
         }
 
         if (enabled != null) {
@@ -65,10 +103,6 @@ public class UiSelectorParser {
             selector = selector.longClickable(longClickable);
         }
 
-        if (packageName != null) {
-            selector = selector.packageName(packageName);
-        }
-
         if (scrollable != null) {
             selector = selector.scrollable(scrollable);
         }
@@ -77,14 +111,89 @@ public class UiSelectorParser {
             selector = selector.selected(selected);
         }
 
-        if (text != null) {
-            selector = selector.text(text);
-        }
-
         if (resourceId != null) {
             selector = selector.resourceId(resourceId);
         }
 
         return selector;
+    }
+
+    private static UiSelector setUiSelectorClassNameWithSelectionOption(UiSelector uiSelector, Pair<String, UiElementSelectionOption> propertyPair) {
+        switch (propertyPair.getValue()) {
+            case CONTAINS:
+                String pattern = "^.*%s.*$";
+                uiSelector = uiSelector.classNameMatches(String.format(pattern, propertyPair.getKey()));
+                break;
+            case EQUALS:
+                uiSelector = uiSelector.className(propertyPair.getKey());
+                break;
+            case WORD_MATCH:
+                uiSelector = uiSelector.classNameMatches(propertyPair.getKey());
+                break;
+            default:
+                uiSelector = uiSelector.className(propertyPair.getKey());
+                break;
+        }
+
+        return uiSelector;
+    }
+
+    private static UiSelector setUiSelectorPackageWithSelectionOption(UiSelector uiSelector, Pair<String, UiElementSelectionOption> propertyPair) {
+        switch (propertyPair.getValue()) {
+            case CONTAINS:
+                String pattern = "^.*%s.*$";
+                uiSelector = uiSelector.packageNameMatches(String.format(pattern, propertyPair.getKey()));
+                break;
+            case EQUALS:
+                uiSelector = uiSelector.packageName(propertyPair.getKey());
+                break;
+            case WORD_MATCH:
+                uiSelector = uiSelector.packageNameMatches(propertyPair.getKey());
+                break;
+            default:
+                uiSelector = uiSelector.packageName(propertyPair.getKey());
+                break;
+        }
+
+        return uiSelector;
+    }
+
+    private static UiSelector setUiSelectorTextWithSelectionOption(UiSelector uiSelector, Pair<String, UiElementSelectionOption> propertyPair) {
+        switch (propertyPair.getValue()) {
+            case CONTAINS:
+                uiSelector = uiSelector.textContains(propertyPair.getKey());
+                break;
+            case EQUALS:
+                uiSelector = uiSelector.text(propertyPair.getKey());
+                break;
+            case WORD_MATCH:
+                uiSelector = uiSelector.textMatches(propertyPair.getKey());
+                break;
+            default:
+                uiSelector = uiSelector.text(propertyPair.getKey());
+                break;
+
+        }
+
+        return uiSelector;
+    }
+
+    private static UiSelector setUiSelectorDescriptionWithSelectionOption(UiSelector uiSelector, Pair<String, UiElementSelectionOption> propertyPair) {
+        switch (propertyPair.getValue()) {
+            case CONTAINS:
+                uiSelector = uiSelector.descriptionContains(propertyPair.getKey());
+                break;
+            case EQUALS:
+                uiSelector = uiSelector.description(propertyPair.getKey());
+                break;
+            case WORD_MATCH:
+                uiSelector = uiSelector.descriptionMatches(propertyPair.getKey());
+                break;
+            default:
+                uiSelector = uiSelector.description(propertyPair.getKey());
+                break;
+        }
+
+        return uiSelector;
     }
 }
